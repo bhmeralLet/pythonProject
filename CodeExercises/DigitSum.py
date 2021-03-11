72ad5d715e3e8dece12be2781f52a2fe6bf88846
bhmeralLet <bahameral@hotmail.com>
2021-03-11 11:37:22 -0800

    update on digit_sum

--- CodeExercises/DigitSum.py
+++ CodeExercises/DigitSum.py
@@ -1,4 +1,5 @@
-
+# needs debugging for numbers that decimal point is bigger than or equal to 5
+# it rounds the number and result gets more
 def sum_of_digits(number):
     total = 0
     while number > 0:
@@ -8,5 +9,5 @@ def sum_of_digits(number):
         number = int("{:.0f}".format(number))
     print(total)
 
-sum_of_digits(36)
+sum_of_digits(22)
 
